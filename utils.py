5850b6765c922acc7e3cf609cf1397199d9252e1
Jathurshan Pradeepkumar <jathurshanpradeepkumar@gmail.com>
2022-02-27 03:41:31 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -24,3 +24,16 @@ def interactive_plot(img_list,title_list, cmap = 'gray'):
     plt.show()
   select_slice = IntSlider(min=0, max=img_list[0].shape[0]-1, description='Select Slice', continuous_update=False)
   return interactive(plotting_images, slice=select_slice)
+
+
+def get_merged_img(img1, img2,num_regions = 4):
+  img_new = itk.array_from_image(img1)
+  if img1.shape[0]!=img2.shape[0]:
+    img2 = itk.image_from_array(img2[:img1.shape[0]])
+  size = img1.shape[-1]//num_regions
+  for i in range (num_regions):
+    for j in range(num_regions):
+      if (i+j)%2 == 1:
+        img_new[:,i*size:(i+1)*size,j*size:(j+1)*size] = itk.array_from_image(img2)[:,i*size:(i+1)*size,j*size:(j+1)*size]
+
+  return img_new
