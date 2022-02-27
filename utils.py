926fd8da3384bcf94bf5ddf5efa3286b0c70124f
Jathurshan Pradeepkumar <jathurshanpradeepkumar@gmail.com>
2022-02-27 14:09:57 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style('darkgrid')
 from seaborn.matrix import clustermap
+from matplotlib.animation import FuncAnimation
+from IPython import display
 
 def plotting_images(img_list,title_list, cmap = 'gray'):
   fig, ax = plt.subplots(1,len(img_list), figsize=(20,8))
@@ -37,3 +39,31 @@ def get_merged_img(img1, img2,num_regions = 4):
         img_new[:,i*size:(i+1)*size,j*size:(j+1)*size] = itk.array_from_image(img2)[:,i*size:(i+1)*size,j*size:(j+1)*size]
 
   return img_new
+
+def interactive_gif_data(img, cmap = 'gray'):
+  """
+  Interactive tool to visualize the dataset as gif. 
+  """
+  def Animate():
+    Figure = plt.figure() 
+    # creating a plot
+    imgs_plotted = plt.imshow(img[0,:,:], cmap = 'gray') 
+    plt.axis("off")
+
+    def AnimationFunction(frame):
+        y = img[frame,:,:]
+    
+        # line is set with new values of x and y
+        imgs_plotted.set_data(y)
+
+    anim_created = FuncAnimation(Figure, AnimationFunction, frames=img.shape[0]-1, interval=200)
+
+    video = anim_created.to_html5_video()
+    html = display.HTML(video)
+    display.display(html)
+    # plt.show()
+  # good practice to close the plt object.
+    plt.close()
+
+
+  return interactive(Animate)
